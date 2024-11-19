fdc47d7aef92de319cb287afbb784993d8ff2a1f
Maria Lourdusamy <maria.lourdusamy@gmail.com>
2024-11-18 19:58:23 -0600

    Commit of Core Java - Typecasting.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        OperatorsNumbers opObj = new OperatorsNumbers();
+        opObj.useLeftShift();
+        opObj.useRightShift();
+        opObj.useBitwiseAndOperator();
+        opObj.useBitwiseOROperator();
+        opObj.usePostfixOperator();
+        opObj.useUnaryOperators();
+    }
+}
