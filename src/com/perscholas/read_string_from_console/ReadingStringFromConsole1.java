36b0460d5091d707776c9d93cd12a6b3d2196354
Maria Lourdusamy <maria.lourdusamy@gmail.com>
2024-11-20 14:40:39 -0600

    Commit Read String from Console

---
+++ src/com/perscholas/read_string_from_console/ReadingStringFromConsole1.java
@@ -0,0 +1,13 @@
+package com.perscholas.read_string_from_console;
+import java.util.Scanner;
+
+public class ReadingStringFromConsole1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        String s = input.nextLine();
+        char ch = s.charAt(0);
+        System.out.println("The character entered is " + ch);
+    }
+}
+
