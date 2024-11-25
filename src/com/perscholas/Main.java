5e2d4c1d4975c402638d67b6a7d081170cd1ebbf
Maria Lourdusamy <maria.lourdusamy@gmail.com>
2024-11-25 14:32:31 -0600

    Commit Customer Management Check-in.

---
+++ src/com/perscholas/Main.java
@@ -0,0 +1,10 @@
+package com.perscholas;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Customer obj = new Customer();
+        obj.inputCustomerData();
+        obj.displayCustomerData();
+    }
+}
